b3db720681db740c6c3485e1ffd27ff5475dc892
Forrest "Zepher" Griffin <fpg4@humboldt.edu>
2023-06-30 22:39:36 -0700

    adding css and icons attempt

---
+++ js/scripts.js
@@ -0,0 +1,46 @@
+/*  Copyright 2014 Kohu
+    This work is free. You can redistribute it and/or modify it under the
+    terms of the Do What The Fuck You Want To Public License, Version 2,
+    as published by Sam Hocevar. See the COPYING file for more details. */
+
+/*  Remove mascot class from element "main" if its width is <= mascotMinWidth */
+function controlMascot(mascot, mascotMinWidth) {
+    $(window).resize(function(event) {
+        if ( $(window).width() <= mascotMinWidth && $("main").hasClass("mascot") ) {
+            removeMascot();
+        } else if( $(window).width() > mascotMinWidth && ! $("main").hasClass("mascot") ) {
+            setMascot(mascot);
+        }
+    });
+}
+
+function setMascot(mascot) {
+    $('main').addClass("mascot");
+    $('main').css("background-image", "url(" + mascot + ")");
+    $('main').removeClass("plain");
+}
+
+function removeMascot() {
+    $('main').removeClass("mascot");
+    $('main').css("background-image", "");
+    $('main').addClass("plain");
+}
+
+$(document).ready(function(event) {
+    var mascotEnable    = true;
+    var mascotPath      = "images/mascots/"
+    var mascotList = ['flan1.png', 'flan2.png', 'flan3.png', 'flan4.png', ];
+
+    var mascot          = mascotPath + mascotList[Math.floor(Math.random() * mascotList.length)];
+    var mascotMinWidth  = '750';
+
+    if ( mascotEnable ) { 
+        setMascot(mascot);
+        controlMascot(mascot, mascotMinWidth);
+    } else { removeMascot(); }
+});
+function vgsearch() {  //Moot u cheeky little kunt why do I have to make a function for this kind of crap? Script to search the /vg/ board.
+    var url = "https://boards.4chan.org/vg/";
+    url += $('#vgresult').val();
+    window.location = url;
+};
